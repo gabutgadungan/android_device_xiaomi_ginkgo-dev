45d3bdbf2d0935d88dda0d178eb26cdc068d7b4b
Martin Liu <liumartin@google.com>
2021-07-19 10:02:01 +0000

    ginkgo: Add filecache_min_kb tuning for lmkd
    Set filecache_min_kb to 150MB according to our
    test data.
    
    Bug: 193293513
    Change-Id: I99381d9ef024b18249ebb21dc61e4668dbe1cb31
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: WisnuArdhi28 <wisnuardi128@gmail.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -232,6 +232,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# LMKD
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.lmk.filecache_min_kb=153600
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.ginkgo
